187d302e6990cb56aab5e282544622e2344c1726
ghiermann <gerhard.hiermann@nuwings.at>
2016-07-29 13:13:38 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Lemmings-Bot',
+      version='1.0',
+      description='Event Bot for lemmings.io',
+      author='nuwings',
+      author_email='software@nuwings.at',
+      url='https://github.com/moccadroid/lemmings-bot'
+      )
